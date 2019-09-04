d4e4a4f04bf3ec8872f710200d2bea045da0aff3
cjw363 <asd6555363>
2019-09-04 16:16:29 +0800

    修bug

--- src/main/java/com/ts/cpfr/websocket/WebSocketConfig.java
+++ src/main/java/com/ts/cpfr/websocket/WebSocketConfig.java
@@ -3,6 +3,8 @@ package com.ts.cpfr.websocket;
 import com.ts.cpfr.spring.WebSocketInterceptor;
 import com.ts.cpfr.utils.SystemConfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -20,19 +22,19 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
 
+    private Logger mLogger = LoggerFactory.getLogger(WebSocketConfig.class);
     //需要用注入的方式，SocketMessageHandle才能交给spring管理
     @Autowired
     SocketMessageHandle handle;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        System.out.println("==========================socket配置==========================");
+        mLogger.info("==========================socket配置==========================");
         //注册websocket server实现类，"/webSocketServer"访问websocket的地址
-        registry.addHandler(handle, SystemConfig.WEB_SOCKET_URL)
-          .addInterceptors(new WebSocketInterceptor());
+        registry.addHandler(handle, SystemConfig.WEB_SOCKET_URL).addInterceptors(new WebSocketInterceptor());
         //使用socketjs的注册方法 兼容低端浏览器,不加还不行？
-//        registry.addHandler(handle, SystemConfig.WEB_SOCKET_URL + "/sockjs")
-//          .addInterceptors(new WsHandshakeInterceptor())
-//          .withSockJS();
+        //        registry.addHandler(handle, SystemConfig.WEB_SOCKET_URL + "/sockjs")
+        //          .addInterceptors(new WsHandshakeInterceptor())
+        //          .withSockJS();
     }
 }
